0a60750bda6777cb1acd07c17a183e3cf1b70366
elinaelkama <elina.elkama@hotmail.com>
2022-12-22 16:23:05 +0200

    fixed typo, add flavor

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ async def on_member_join(member):
 async def choose(ctx, *args):
     arguments = " ".join(args).split(",")
     choices = list(map(lambda choice: choice.strip().capitalize(), arguments))
-    flavor = ["is by far the best choice", "and if you pisk anything else I'll never talk to you again", "try it, you'll love it", "is clearly the only choice here"]
+    flavor = ["is by far the best choice", "and if you pick anything else I'll never talk to you again", "try it, you'll love it", "is clearly the only choice here", "is best by all measures"]
     await ctx.send(f'**{random.choice(choices)}** {random.choice(flavor)}.')
 
 @bot.command()
