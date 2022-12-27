1a1f042145c712b3d8ecb4d184e3c050cb92d256
elinaelkama <elina.elkama@hotmail.com>
2022-12-27 22:47:36 +0200

    ?

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def on_member_join(member):
 
 #Choses a random item from the list given
 @bot.command()
-async def choose(ctx, *args):
+async def choose(ctx, *args)
     arguments = " ".join(args).split(",")
     choices = list(map(lambda choice: choice.strip().capitalize(), arguments))
     flavor = ["is by far the best choice", "and if you pick anything else I'll never talk to you again", "try it, you'll love it", "is clearly the only choice here", "is best by all measures"]
