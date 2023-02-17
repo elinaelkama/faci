61dd2bf9fc10e569c28f7ca8c93d6484b5e62ae4
elinaelkama <elina.elkama@hotmail.com>
2023-02-17 17:42:15 +0200

    add userJoined command, refined ScheduledEventUpdate

--- commands/userJoined.py
+++ commands/userJoined.py
@@ -1,4 +1,8 @@
 import discord
+from datetime import datetime, timedelta
+import humanfriendly
 
 async def joined(member: discord.Member):
-	return f'{member.name} joined on {member.joined_at.strftime("%a %d.%m.%Y at %H:%M")}'
+	timezone = member.joined_at.tzinfo
+	diff: timedelta = datetime.now(timezone) - member.joined_at
+	return f'{member.name}, you joined on {member.joined_at.strftime("%a %d.%m.%Y at %H:%M")} UTC/GMT\nYou have been on the server for {humanfriendly.format_timespan(diff)}'
