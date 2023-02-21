e229f69599441d4063b8e25ead52e2bea109d0c3
elinaelkama <elina.elkama@hotmail.com>
2023-02-21 14:23:34 +0200

    commandHandler tests done

--- commandHandlers/testCommandHandler.py
+++ commandHandlers/testCommandHandler.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch
 
-from commandHandlers import randomChoice
+from commandHandlers import guildCreated, randomChoice, userJoined
 from helpers import testHelper
 #, side_effects=["dog", "try it, you'll love it.", "everything everywhere all at once", "is by far the best choice."]
 class CommandHandlerTest(unittest.IsolatedAsyncioTestCase):
@@ -19,4 +19,16 @@ class CommandHandlerTest(unittest.IsolatedAsyncioTestCase):
 
     async def testRandomChoiceSad(self):
         self.assertEqual(await randomChoice.randomChoice(), "You have given me no choice")
-        self.assertEqual(await randomChoice.randomChoice(*testHelper.parseRawCommand("   ,  , , ,  ,,,  , , , , ")), "You have given me no choice")
+        self.assertEqual(await randomChoice.randomChoice(*testHelper.parseRawCommand("   ,  , , ,  ,,,  , , , , ")),
+            "You have given me no choice"
+            )
+
+    async def testGuildCreated(self):
+        self.assertEqual(await guildCreated.guildCreated(testHelper.getGuildMock()),
+            "Murder Mittens Inc was created on 27.01.2023 UTC/GMT\nServer has been in existence for 3 weeks, 3 days and 14 hours"
+            )
+
+    async def testUserJoined(self):
+        self.assertEqual(await userJoined.userJoined(testHelper.getMemberMock()),
+            "huuhaakettu, you joined on Fri 27.01.2023 at 22:12 UTC/GMT\nYou have been on the server for 3 weeks, 3 days and 14 hours"
+            )
--- helpers/testHelper.py
+++ helpers/testHelper.py
@@ -1,5 +1,32 @@
+from datetime import datetime
+from unittest.mock import MagicMock
+import discord
+import pytz
+
+
 def parseRawCommand(command: str):
 	return command.split(" ")
 
 def getRandomChoiceMock(i: int):
 	return lambda choices: choices[i]
+
+def getDatetimeNowMock():
+	return datetime(2023, 5, 14, 13, 45, 20, 12)
+
+def getDatetimeFutureMock():
+	return datetime(2023, 7, 7, 6, 30, 31, 47, pytz.UTC)
+
+def getDatetimePastMock():
+	return datetime(2023, 1, 27, 22, 12, 36, 21, pytz.UTC)
+
+def getGuildMock():
+	mockGuild = MagicMock(spec=discord.Guild)
+	mockGuild.name = "Murder Mittens Inc"
+	mockGuild.created_at = getDatetimePastMock()
+	return mockGuild
+	
+def getMemberMock():
+	mockMember = MagicMock(spec=discord.Member)
+	mockMember.name = "huuhaakettu"
+	mockMember.joined_at = getDatetimePastMock()
+	return 	mockMember
