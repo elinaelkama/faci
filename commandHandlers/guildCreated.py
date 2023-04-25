5a38ab37b7d9734410377ae3b7bbaf91cade462a
elinaelkama <elina.elkama@hotmail.com>
2023-04-25 22:22:57 +0300

    event time fix

--- commandHandlers/guildCreated.py
+++ commandHandlers/guildCreated.py
@@ -4,7 +4,7 @@ import humanfriendly
 
 
 async def guildCreated(guild: discord.Guild):
-    timezone = guild.created_at.tzinfo
+    timezone = guild.created_at.astimezone(datetime.timezone.utc)
     diff: datetime.timedelta = datetime.datetime.now(
         timezone) - guild.created_at
     return f'{guild.name} was created on {guild.created_at.strftime("%d.%m.%Y")}\nServer has been in existence for {humanfriendly.format_timespan(diff)}'
--- commandHandlers/userJoined.py
+++ commandHandlers/userJoined.py
@@ -4,7 +4,7 @@ import humanfriendly
 
 
 async def userJoined(member: discord.Member):
-    timezone = member.joined_at.tzinfo
+    timezone = member.joined_at.astimezone(datetime.timezone.utc)
     diff: datetime.timedelta = datetime.datetime.now(
         timezone) - member.joined_at
     return f'{member.name}, you joined on {member.joined_at.strftime("%a %d.%m.%Y")}\nYou have been on the server for {humanfriendly.format_timespan(diff)}'
