cea6f03376e1404818ceda7105e1da31ec4e99a5
elinaelkama <elina.elkama@hotmail.com>
2023-06-06 16:27:19 +0300

    Fix: user being kicked/banned with no reason given no longer returns None

--- eventHandlers/memberRemove.py
+++ eventHandlers/memberRemove.py
@@ -4,7 +4,11 @@ import discord
 async def memberRemove(member: discord.Member):
     async for entry in member.guild.audit_logs(limit=1):
         if entry.action == discord.AuditLogAction.kick:
+            if entry.reason == None:
+                return f'{member.name}#{member.discriminator} was kicked from the server'
             return f'{member.name}#{member.discriminator} was kicked from the server for: {entry.reason}'
         if entry.action == discord.AuditLogAction.ban:
+            if entry.reason == None:
+                return
             return f'They were banned from the server for: {entry.reason}'
     return f'{member.name}#{member.discriminator} has left the server'
